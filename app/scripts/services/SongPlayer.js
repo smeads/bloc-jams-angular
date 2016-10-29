6774e699b6a7acad5a79420deffa384f1b1c5082
Steven Meads <sbmeads@gmail.com>
2016-10-28 20:49:30 -0700

    Add seekBar update time

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-   function SongPlayer(Fixtures) {
+   function SongPlayer($rootScope, Fixtures) {
       var SongPlayer = {};
       
       /**
@@ -26,6 +26,13 @@
       SongPlayer.currentSong = null;
       
       /**
+      * @desc Current playback time (in seconds) of currently playing song
+      * @type {Number}
+      */
+      
+      SongPlayer.currentTime = null;
+      
+      /**
       * @desc Buzz object audio file
       * @type {Object}
       */
@@ -46,6 +53,12 @@
          currentBuzzObject = new buzz.sound(song.audioUrl, {
             formats: ['mp3'],
             preload: true
+         });
+         
+         currentBuzzObject.bind('timeupdate', function() {
+            $rootScope.$apply(function() {
+               SongPlayer.currentTime = currentBuzzObject.getTime();
+            });
          });
          
          SongPlayer.currentSong = song;
@@ -136,11 +149,23 @@
          }
       };
       
+      /**
+      * @function setCurrentTime
+      * @desc Set current time (in seconds) of currently playing song
+      * @param {Number} time
+      */
+      
+      SongPlayer.setCurrentTime = function(time) {
+         if (currentBuzzObject) {
+            currentBuzzObject.setTime(time);
+         }
+      };
+      
       
       return SongPlayer;
    }
    
    angular
       .module('blocJams')
-      .factory('SongPlayer', SongPlayer);
+      .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
 })();
